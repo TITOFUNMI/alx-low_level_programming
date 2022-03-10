2f9a854f0bb8bbfd230ca9a73347e2dd2da0ab90
root <olayiwolatofunmi212@gmail.com>
2022-03-10 09:57:40 -0800

    message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,9 @@
 /**
  * main - Entry point
  *
- * int filedes:Always 2 (Success)
- * constant char  for the text *buf
- * date = nbyte
- *
  * Return: Always 1 (Success)
  *
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
