f0050c7d4663f1b5a11d996290fd77e077efeb38
root <olayiwolatofunmi212@gmail.com>
2022-03-11 08:58:53 -0800

    if-else-while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int num;
+
+	for(num = 0; num <= 9; num++)
+		printf("%d", num);
+	printf("\n");
+
+	return (0);
+
+}
+
