5aa63a181cf1ced25725c553a0dfc38fb2e46a77
root <olayiwolatofunmi212@gmail.com>
2022-04-14 09:41:52 -0700

    addition

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,16 +15,8 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	sum = 0;
-	if (n == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-			sum += va_arg(ap, int);
-	}
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
 	va_end(ap);
 	return (sum);
