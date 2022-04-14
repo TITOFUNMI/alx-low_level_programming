4a2b81d0b130158bd3131c07c08ed63d4235a457
root <olayiwolatofunmi212@gmail.com>
2022-04-14 09:52:03 -0700

    variadic function

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FUNCTION_H_
+#define VARIADIC_FUNCTION_H_
+
+#include <stdarg.h>
+/**
+typedef struct printer
+
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+*/
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /*Variadic Functions*/
