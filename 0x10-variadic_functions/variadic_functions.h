722f363df51b5e8e7ac25c0841985bbbb12200cf
root <olayiwolatofunmi212@gmail.com>
2022-04-14 10:46:02 -0700

    print number

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -21,5 +21,8 @@ typedef struct printer
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /*Variadic Functions*/
